2e8cf013a6e69664f6ca083aa4e10a4391ba2c8b
mactive <mactive@gmail.com>
2018-02-09 11:28:49 +0800

    02ts

---
+++ es6-algorithm/02_selection_sort.ts
@@ -0,0 +1,35 @@
+// Selection Sort - O(log n^2)
+// Parameter:
+// 1. random array
+
+// 1. Finds the smallest value in an array
+const findSmallestIndex = (array: number[]) => {
+  let smallestElement = array[0];
+  let smallestIndex = 0;
+  for (let i =1; i < array.length; i++) {
+    if(array[i] < smallestElement) {
+      smallestElement = array[i];
+      smallestIndex = i;
+    }
+  }
+  return smallestIndex;
+}
+
+// 2. sorts the array
+const selectionSort = (array: number[]) => {
+  const sortedArray = [];
+  // 这一步很必要, 因为下面会改变array的长度
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    const smallestIndex = findSmallestIndex(array);
+    // get the element and remove it from array
+    const smallestElement = array.splice(smallestIndex,1)[0];
+    console.log('array',array);
+    sortedArray.push(smallestElement);
+  }
+  return sortedArray;
+}
+
+let array = [3, 5, 8, 2, 1, 0.2];
+console.log(selectionSort(array));
