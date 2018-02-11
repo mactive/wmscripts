7ad506a4b7d451502ffda273b79a0ec7ce45dfc1
mactive <mactive@gmail.com>
2018-02-11 14:53:51 +0800

    hash function

---
+++ es6-algorithm/05_hashtable_time33.js
@@ -0,0 +1,11 @@
+"use strict";
+const genHash = (str = '') => {
+    let hash = 5381;
+    if (!str)
+        return null;
+    for (let i = 0; i < str.length; i++) {
+        hash += (hash << 5) + str.charCodeAt(i);
+    }
+    return hash && 0x7fffffff;
+};
+console.log(genHash('aaaa'));
